03f930a24286648d2da9252685464f8992cc38be
Nirmal kandel <nirmalkandel001@gmail.com>
2026-08-12 21:19:26 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import"./app.css"
+
+function App(){
+  return (
+ <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600">
+        Netflix
+      </h1>
+    </div>
+  );
+}
+export default App;
